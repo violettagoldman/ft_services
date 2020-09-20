be4983fa0798045104b95bd28b159f4da15833ab
violettagoldman <violagoldman@mail.ru>
2020-09-20 12:14:11 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ "$1" == "clean" ]];
+then
+	kubectl delete --all deployments
+	kubectl delete --all pods
+	kubectl delete --all services
+#A PersistentVolumeClaim (PVC) is a request for storage by a user. It is similar to a Pod. Pods consume node resources and PVCs consume PV resources.
+	kubectl delete --all pvc
+else
+	#Change defautl driver to a docker one
+	minikube start --driver=docker
+	#Link local docker to vm docker
+	eval $(minikube docker-env)
+	#setup metallb
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+	kubectl apply -f srcs/metallb.yml
+# On first install only
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+	#Build nginx
+	docker build srcs/nginx/. -t nginx
+	#Create pod and service
+	kubectl create -f srcs/nginx.yml
+	#launch dashboard
+	minikube dashboard &
+fi
