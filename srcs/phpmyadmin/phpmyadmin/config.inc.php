695a662adf82a8f0dc478e97980ef15f551d0645
violettagoldman <violagoldman@mail.ru>
2020-10-23 19:16:00 +0200

    fix: reverse proxy

--- srcs/phpmyadmin/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/phpmyadmin/config.inc.php
@@ -14,7 +14,8 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+#$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['PmaAbsoluteUri'] = '/phpmyadmin';
 /**
  * Servers configuration
  */
